d4e6412f3697ca808f3a80d7bfef53aaf22a3bcc
EM_SSR <124417055+ME-SSR@users.noreply.github.com>
2023-10-06 05:46:16 +0900

    U字型のネットワークを作ってくれるよ

--- SEN/network/example.py
+++ SEN/network/example.py
@@ -7,12 +7,10 @@ from bindsnet.analysis.plotting import plot_spikes, plot_voltages
 from bindsnet.analysis.visualization import summary
 
 # Simulation time.
-time = 500
-
-network = Network(dt = 1.0, batch_size = 1)
+time = 1000
 
 # Create the network.
-network = UShapedPipeTopology(dt = 1.0 , batch_size = 1, num_neurons = 40, shape = ([1,2]), radius = 2, u_depth = 0.1 )
+network = UShapedPipeTopology(dt = 1.0 , batch_size = 10, num_neurons = 5, shape = ([15,15]), radius = 2, u_depth = 0.1 )
 u_shaped_network = network.build()  # これでネットワークのインスタンスを取得します
 
 visualize_network(u_shaped_network)
@@ -25,27 +23,28 @@ source_monitor = Monitor(
     time=time,  # Length of simulation (if known ahead of time).
 )
 target_monitor = Monitor(
-    obj=u_shaped_network.Neuron_14,
-    state_vars=("s"),  # Record spikes and voltages.
+    obj=u_shaped_network.Neuron_4,
+    state_vars=("s", "v"),  # Record spikes and voltages.
     time=time,  # Length of simulation (if known ahead of time).
 )
 
-u_shaped_network.add_monitor(monitor=source_monitor, name="A")
-u_shaped_network.add_monitor(monitor=target_monitor, name="B")
+u_shaped_network.add_monitor(monitor=source_monitor, name="Neuron_0")
+u_shaped_network.add_monitor(monitor=target_monitor, name="Neuron_39")
 
 # Create input spike data, where each spike is distributed according to Bernoulli(0.1).
 input_data = torch.bernoulli(0.1 * torch.ones(time, u_shaped_network.Neuron_0.n)).byte()
-inputs = {"A": input_data}
+inputs = {"Neuron_0": input_data}
 
 # Simulate network on input data.
 u_shaped_network.run(inputs=inputs, time=time)
 
 # Retrieve and plot simulation spike, voltage data from monitors.
 spikes = {
-    "A": source_monitor.get("s"), "B": target_monitor.get("s")
+    "Neuron_0": source_monitor.get("s"), "Neuron_39": target_monitor.get("s")
 }
+voltages = {"Neuron_39": target_monitor.get("v")}
 
 plt.ioff()
 plot_spikes(spikes)
-#plot_voltages(voltages, plot_type="line")
+plot_voltages(voltages, plot_type="line")
 plt.show()
